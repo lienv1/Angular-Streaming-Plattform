f1c483def2c8d7c213216e43a02e0b71d035661d
lienv1 <bobby_lien@hotmail.com>
2023-01-24 02:16:39 +0100

    Create public branch

--- src/app/model/Actor.ts
+++ src/app/model/Actor.ts
@@ -2,14 +2,8 @@ import { Movie } from "./Movie";
 
 export interface Actor{
     actorName:string;
-    actorNameJap:string;
     birthday:Date;
 	bloodtype:string;
-	height:number;
-	hip:number;
-	breast:number;
-	waist:number;
-	ethnicity:string;
     rating:number;
 	comment:string;
 	added:Date;
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: "http://0.0.0.0:8080"
+  apiBaseUrl: "http://localhost:8080"
 };
 
 /*
